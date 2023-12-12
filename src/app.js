4d3d60af5009bef31504bee51f2321234850ba93
Dana M <147849932+dkfmedia@users.noreply.github.com>
2023-12-12 01:09:02 +0000

    I had forgotten to save this

--- src/app.js
+++ src/app.js
@@ -5,7 +5,28 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+const initialize = () => {
+  const colorName = document.querySelector(".color");
+  const changeBtn = document.querySelector("#btn");
+
+  function changeColor() {
+    let r = Math.floor(Math.random() * 256);
+    let g = Math.floor(Math.random() * 256);
+    let b = Math.floor(Math.random() * 256);
+
+    let color = `rgb(${r}, ${g}, ${b})`;
+
+    //change the body's background color to the new generated color and also show it the span text content
+    document.body.style.backgroundColor = color;
+    colorName.textContent = color;
+  }
+
+  //we call the changeColor function every time we click on the button
+
+  changeBtn.addEventListener("click", changeColor);
 };
+
+//onlad function is really important as it ensures that the code starts running when the page has loaded
+window.onload = initialize;
+
+console.log("Hi there, thanks for checking me out!");
